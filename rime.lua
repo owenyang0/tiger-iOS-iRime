8f5ba5dcb67eb4832d5c33b8950a86b7754439d5
yangsong13 <yangsong13@meituan.com>
2023-04-10 19:51:12 +0800

    update to 230401

--- rime.lua
+++ rime.lua
@@ -4,5 +4,5 @@ core2022 = require("core2022_filter") --自定义字符集过滤（常用字集�
 number_translator = require("number")
 lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
 calculator_translator = require("calculator_translator")  --简易计算器
-exe_processor = require("exe")  -- 网页启动器
+-- exe_processor = require("exe")  -- 网页启动器
 shijian2_translator = require("shijian2") -- 高级时间
